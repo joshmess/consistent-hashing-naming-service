057a367d543c9e67ae4a5cc9776d83e6540f3772
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2021-04-25 13:44:31 -0400

    Add entry for one nameserver

--- Nameserver/src/Nameserver.java
+++ Nameserver/src/Nameserver.java
@@ -1,11 +1,14 @@
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Nameserver {
 
-    private NSConfig configuration;
-    private static Socket socket;
+    NSConfig configuration;
+    static Socket socket;
+    HashMap<Integer, String> pairs;
 
     public Nameserver(int id,int conn_port){
+        pairs = new HashMap<>();
         configuration = new NSConfig(id, conn_port);
     }
 
