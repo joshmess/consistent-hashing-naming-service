7a96050a83c21c08950a9f19c037a701e03911fe
Josh <joshmessitte@gmail.com>
2021-04-28 13:33:20 -0400

    Testing

--- Bootsrap/src/BootstrapDriver.java
+++ Bootsrap/src/BootstrapDriver.java
@@ -20,7 +20,7 @@ public class BootstrapDriver {
         int bootstrap_conn_port = Integer.parseInt(config_scan.nextLine());
         ServerSocket ss = new ServerSocket(bootstrap_conn_port);
         int highest_ns_id = 0;
-        
+
         //Create Bootstrap server
         Bootstrap bootstrap_ns = new Bootstrap(bootstrap_id);
 
@@ -34,7 +34,7 @@ public class BootstrapDriver {
         UIThread bootstrap_ui = new UIThread(bootstrap_ns);
         bootstrap_ui.start();
 
-        //Bootstrap main process waiting for more nameservers
+        //Bootstrap server process waiting for more nameservers
         while(true){
 
             Socket ns_socket = ss.accept();
@@ -220,5 +220,57 @@ public class BootstrapDriver {
             highest_ns_id = Collections.max(bootstrap_ns.server_list);
         }
 
+    }
+    /*
+    * This class represents the UI for a Bootstrap Nameserver
+    */
+    private static class UIThread  extends Thread{
+
+        Bootstrap bootstrap_ns;
+
+        // Default Constructor
+        public UIThread(Bootstrap bootstrap_ns){
+            this.bootstrap_ns = bootstrap_ns;
+        }
+
+        public void run(){
+
+            Scanner scan = new Scanner(System.in);
+            String query = "";
+
+            while(!query.equals("quit")) {
+
+                System.out.print("Bootstrap-NS>_");
+                query = scan.nextLine();
+                String[] query_list = query.split(" ");
+
+                if(query_list[0].equals("lookup")){
+
+                    try {
+                        System.out.println(bootstrap_ns.lookup(Integer.parseInt(query_list[1])));
+                    } catch (NumberFormatException | ClassNotFoundException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }else if(query_list[0].equals("insert")){
+
+                    try {
+                        bootstrap_ns.insert(Integer.parseInt(query_list[1]), query_list[2]);
+                    } catch (NumberFormatException | ClassNotFoundException | IOException e) {
+                        e.printStackTrace();
+                    }
+    
+                }else if(query_list[0].equals("delete")){
+
+                
+                    try {
+                        bootstrap_ns.delete(Integer.parseInt(query_list[1]));
+                    } catch (NumberFormatException | ClassNotFoundException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+                    System.out.println(">_Query Not Recognized");
+                }
+            }
+        }
     }
 }
