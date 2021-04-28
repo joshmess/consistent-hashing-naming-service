7a3da7953441f7e4721b5416a1ac9bec84460a7f
Josh <joshmessitte@gmail.com>
2021-04-28 14:08:34 -0400

    Merge NSConfig File

--- Nameserver/src/Nameserver.java
+++ Nameserver/src/Nameserver.java
@@ -62,5 +62,45 @@ public class Nameserver {
             return ">_FAIL";
         }
     }
+    /*
+    * Thisprivate inner class stores information about each nodes predecessor and successor
+    */
+    private class NSConfig {
+
+        int id;
+        int conn_port;
+
+        //Predecessor info
+        String predecessor_ip;
+        int predecessor_port;
+        int predecessor_id;
+
+        //Successor info
+        String successor_ip;
+        int successor_port;
+        int successor_id;
+
+        // Default Constructor
+        public NSConfig(int id, int conn_port){
+
+            this.id = id;
+            this.conn_port = conn_port;
+
+            successor_port = 0;
+            successor_id = 0;
+            predecessor_id = 0;
+        }
+
+        // If changes to pred/succ occur
+        public void reconfigure(int successor_port, int predecessor_port, int  successor_id, int predecessor_id, String successor_ip, String predecessor_ip) {
+
+            this.successor_port = successor_port;
+            this.successor_id = successor_id;
+            this.predecessor_id = predecessor_id;
+            this.predecessor_ip = predecessor_ip;
+            this.successor_ip = successor_ip;
+            this.predecessor_port = predecessor_port;
+        }
+    }
 
 }
